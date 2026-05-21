20825d3a62030473c2f83175bdb95490c3797903
tejnath35 <tejnathg35@gmail.com>
2026-05-21 11:04:07 +0530

    a

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,1 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL || (import.meta.env.DEV ? `${window.location.protocol}//${window.location.hostname}:4000` : "https://week5-capstone.onrender.com");
+export const API_URL = import.meta.env.DEV
+  ? "http://localhost:4000"
+  : "https://week5-capstone.onrender.com";
